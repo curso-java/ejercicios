e6c7e934d2d81328fb5c818c46b6b05fba6680a1
curso-java <Curso.NETcic@outlook.com>
2015-09-23 16:15:19 +0200

    Primer ejemplo con maven

---
+++ ejercicio004/src/test/java/com/produban/AppTest.java
@@ -0,0 +1,17 @@
+package com.produban;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void testEjemplo() {
+		assertTrue(true);
+	}
+	
+}
