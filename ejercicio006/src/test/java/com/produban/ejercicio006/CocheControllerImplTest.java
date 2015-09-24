f83b1e38f2225696a75ce22e8d89580ca888a252
curso-java <Curso.NETcic@outlook.com>
2015-09-24 18:01:56 +0200

    Subido el test

---
+++ ejercicio006/src/test/java/com/produban/ejercicio006/CocheControllerImplTest.java
@@ -0,0 +1,45 @@
+package com.produban.ejercicio006;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CocheControllerImplTest {
+
+	private CocheControllerImpl cocheController;
+	
+	private Coche coche;
+	
+	@Before
+	public void setUp() {
+		cocheController = new CocheControllerImpl();
+		
+		CocheServiceImpl cocheService = new CocheServiceImpl();
+		cocheController.setService(cocheService);
+		
+		CocheRepository cocheRepository = new CocheRepositoryImpl();
+		
+		cocheService.setRepository(cocheRepository);
+		
+		coche = new Coche();
+		coche.setColor(5);
+		coche.setConsumo(3.4d);
+	}
+	
+	@Ignore
+	@Test
+	public void testDummy() {
+
+	}
+
+	@Test
+	public void testCreate() {
+		
+		Coche cocheResultado = cocheController.create(coche);
+		
+		assertNotNull("El coche no debería ser nulo", cocheResultado);
+	}
+
+}
